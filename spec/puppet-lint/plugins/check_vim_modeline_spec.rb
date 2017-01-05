8f66c5ed49d2cfe288f4db6f4946d1f9ea5b1055
Richard Guest <r.guest@gns.cri.nz>
2017-01-05 13:28:20 +1300

    Update tests.
    Correct some of the quoting in tests and add a few more tests to catch
    code with trailing newlines.

--- spec/puppet-lint/plugins/check_vim_modeline_spec.rb
+++ spec/puppet-lint/plugins/check_vim_modeline_spec.rb
@@ -16,8 +16,28 @@ describe 'vim_modeline' do
       end
     end
 
+    context 'code with a trailing newline not ending with a vim modeline' do
+      let(:code) { "'test'\n'test'\n" }
+
+      it 'should detect a single problem' do
+        expect(problems).to have(1).problem
+      end
+
+      it 'should create a warning' do
+        expect(problems).to contain_warning(msg).on_line(2).in_column(6)
+      end
+    end
+
+    context 'code with a trailing newline ending with a vim modeline' do
+      let(:code) { "'blah'\n'blah'\n# vim: set ts=2 sw=2 tw=0 et:\n" }
+
+      it 'should not detect any problems' do
+        expect(problems).to have(0).problems
+      end
+	end
+
     context 'code ending with a vim modeline' do
-      let(:code) { "'blah\nblah\n# vim: set ts=2 sw=2 tw=0 et:'\n" }
+      let(:code) { "'blah'\n'blah'\n# vim: set ts=2 sw=2 tw=0 et:" }
 
       it 'should not detect any problems' do
         expect(problems).to have(0).problems
